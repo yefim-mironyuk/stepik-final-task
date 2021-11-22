94b6bbe8a0de96b8b9c5989462f152b9b6162ca8
yefim-mironyuk <efimmironyk@gmail.com>
2021-11-22 15:08:38 +0300

    login_page opens

--- pages/login_page.py
+++ pages/login_page.py
@@ -9,7 +9,7 @@ class LoginPage(BasePage):
         self.should_be_register_form()
 
     def should_be_login_url(self):
-        assert self.current_url.endswith('login/')
+        assert self.browser.current_url.endswith('login/'), 'Wrong url'
 
     def should_be_login_form(self):
         assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form is not presented"
